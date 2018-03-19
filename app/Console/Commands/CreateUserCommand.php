ceddc6a64fed85e5e5a82f34b72a907dcb3192c4
cengiz <onkal.cengiz@gmail.com>
2018-03-19 23:14:37 +0200

    create user

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+
+class CreateUserCommand extends Command
+{
+
+    protected $signature = 'user:create';
+    protected $description = 'Create a user';
+
+    public function handle()
+    {
+        $email=$this->ask('Enter User Email');
+        $password=$this->ask('Enter Password');
+        $validator = \Validator::make(['email'=>$email], [
+            'email' => 'required|email',
+        ]);
+
+        if($validator->fails()){
+            $this->output->writeln($validator->errors()->toJson());
+            exit("bye..\n");
+        }
+
+        $user = new \App\User();
+        $user->password = \Hash::make($password);
+        $user->email = $email;
+        $user->save();
+        $this->output->writeln('User Created');
+    }
+}
