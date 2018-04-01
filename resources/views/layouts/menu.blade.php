1a26fa7944f936348ae09f1f3ad1763785937576
cengiz <onkal.cengiz@gmail.com>
2018-04-01 22:40:51 +0300

    Muhasebe

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -18,7 +18,7 @@
             <ul class="sub-menu children dropdown-menu">
                 <li><i class="fa fa-list"></i><a href="{{url('policy/list')}}">Liste</a></li>
                 <li><i class="fa fa-file"></i><a href="{{url('policy/create')}}">Oluştur</a></li>
-                <li><i class="fa fa-plus"></i><a href="{{url('policy/create_existing')}}">Mevcut Müşteri için poliçe oluştur</a></li>
+                <li><i class="fa fa-plus"></i><a href="{{url('policy/create_existing')}}">Müşteri için oluştur</a></li>
                 <li><i class="fa fa-id-badge"></i><a href="{{url('policy/types')}}">Poliçe Tipleri</a></li>
             </ul>
         </li>
