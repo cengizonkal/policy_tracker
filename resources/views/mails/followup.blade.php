7d4747b38c906106262568ba1980ea6039a79742
cengiz <onkal.cengiz@gmail.com>
2018-05-08 19:52:55 +0300

    add customer type to email

--- resources/views/mails/followup.blade.php
+++ resources/views/mails/followup.blade.php
@@ -26,19 +26,21 @@
 <table>
     <tr>
         <th>Müşteri</th>
+        <th>Müşteri Tipi</th>
         <th>Bitiş Süresi</th>
         <th>Tutar</th>
     </tr>
     @foreach($policies as $policy)
         <tr>
             <td>{{$policy->customer->fullname}}</td>
+            <td>{{$policy->customer->customerType->description}}</td>
             <td>{{$policy->valid_until->format('d/m/Y')}}</td>
             <td>{{$policy->price}}</td>
         </tr>
     @endforeach
 
     <tr>
-        <td colspan="3"><strong>Toplam Poliçe Sayısı : {{$policies->count()}}</strong></td>
+        <td colspan="4"><strong>Toplam Poliçe Sayısı : {{$policies->count()}}</strong></td>
     </tr>
 </table>
 <br>
